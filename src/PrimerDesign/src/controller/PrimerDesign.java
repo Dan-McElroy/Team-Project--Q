e9da04eb55f0c8ffad0a0c61e12e1c4d99af99c6
Ross Barnie <r.barnie@gmail.com>
2012-12-08 13:29:37 +0000

    first working interface design

--- src/PrimerDesign/src/controller/PrimerDesign.java
+++ src/PrimerDesign/src/controller/PrimerDesign.java
@@ -4,10 +4,52 @@
  */
 package controller;
 
+import java.awt.Dimension;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import view.StartPanel;
+
 /**
  *
  * @author 0901758b
  */
 public class PrimerDesign {
+   
+    private static void createAndShowGUI(){
+        // create frame
+        JFrame window = new JFrame("Primer Design");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setPreferredSize(new Dimension(1024, 768));
+        
+        // add start panel to frame
+        StartPanel start = new StartPanel();
+        window.add(start);
+        
+        window.pack();
+        window.setVisible(true);
+    }
     
+    public static void main(String args[]){
+        // set look and feel to same as system
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(PrimerDesign.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(PrimerDesign.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(PrimerDesign.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(PrimerDesign.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
 }
