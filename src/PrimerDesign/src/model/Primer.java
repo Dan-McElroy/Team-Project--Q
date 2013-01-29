5fbd4d227d283db4dbe39f8a49f56616f4b98ad6
Dan McElroy <1002852m@student.gla.ac.uk>
2013-01-29 21:11:13 +0000

    Fixed EVERYTHING.

--- src/PrimerDesign/src/model/Primer.java
+++ src/PrimerDesign/src/model/Primer.java
@@ -21,7 +21,7 @@ public class Primer {
        code = c;
     }
     
-    public TestResult goodLength() {
+public TestResult goodLength() {
     	/*
     	 * True if the primer is of an appropriate length, btwn 20 and 30 bases.
     	 */
@@ -32,11 +32,8 @@ public class Primer {
                 String.valueOf(code.length())));
     }
     
-    public TestResult meltingTemp() {
-    	/*
-    	 * Returns true if the formula to calculate melting temp does not return
-    	 * a value outside the range of 50-65.
-    	 */
+    public Integer getMeltingTemp() {
+        
         int at = 0; int gc = 0;
         for (int i = 0; i < code.length(); i++) {
             char c = code.charAt(i);
@@ -45,7 +42,16 @@ public class Primer {
             else if (c == 'g' || c == 'c')
                 gc++;
         }
-        int meltTemp = (2*at) + (4*gc);
+        return ((2*at) + (4*gc));
+    }
+    
+    public TestResult meltingTemp() {
+    	/*
+    	 * Returns true if the formula to calculate melting temp does not return
+    	 * a value outside the range of 50-65.
+    	 */
+        int meltTemp = getMeltingTemp();
+        
         if (meltTemp >= 50 && meltTemp <= 65)
             return new TestResult(true, (Integer.toString(meltTemp)));
         else
@@ -316,7 +322,6 @@ public class Primer {
     public boolean equals(Primer p) {
         return (this.code.equals(p.getCode()));
     }
-    
     public TestResult test() {
     	/*
     	 * Tests a primer against all the primer rule tests,
@@ -325,15 +330,10 @@ public class Primer {
     	 */
         TestResult t = new TestResult(true, "");
         t.add(meltingTemp());
-        if (!meltingTemp().getPass()) t.setOut(t.getOut() + "#");
         t.add(gcContent());
-        if (!gcContent().getPass()) t.setOut(t.getOut() + "#");
-        t.add(repetition());
-        if (!repetition().getPass()) t.setOut(t.getOut() + "#");
-        t.add(goodLength());
-        if (!goodLength().getPass()) t.setOut(t.getOut() + "#");
-        t.add(selfAnneal());        // Note: need to specify WHERE it splits.
-        if (!selfAnneal().getPass()) t.setOut(t.getOut() + "#");
+        t.add(repetition());                                  //COULD ELAB      
+        t.add(goodLength());                               
+        t.add(selfAnneal());
         return t;
     }
 }
