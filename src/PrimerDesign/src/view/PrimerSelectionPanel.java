3f403ce1ee50f5099680437435164ba663151ec7
RTaylor121 <spud_t_92@hotmail.co.uk>
2013-02-06 10:56:55 +0000

    Coloured highlighting on PrimerSelectionPanel. Still some bugs + line nums not fixed

--- src/PrimerDesign/src/view/PrimerSelectionPanel.java
+++ src/PrimerDesign/src/view/PrimerSelectionPanel.java
@@ -2,6 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package view;
 
 import controller.PrimerDesign;
